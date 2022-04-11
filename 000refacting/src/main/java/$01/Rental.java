5a812e867bedc959541a3e27f5c30cad9fa872d1
huangbenliang <42233168+huangbenliang@users.noreply.github.com>
2022-04-11 19:39:39 +0800

    refacting method

--- 000refacting/src/main/java/$01/Rental.java
+++ 000refacting/src/main/java/$01/Rental.java
@@ -12,4 +12,30 @@ public class Rental {
 		this.daysRented = daysRented;
 	}
 
+    /**
+     * 获取总金额
+     * @return
+     */
+    double getCharge() {
+        double result=0;
+        //determine amounts for aRental line
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (getDaysRented() > 2) {
+                    result += (getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += getDaysRented() * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (getDaysRented() > 3) {
+                    result += (getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }
 }
