d8ee3cdf34ffc8967d778c073afbf71c2237fb7c
huangbenliang <1372263592@qq.com>
2022-05-03 15:25:18 +0800

    reflection:获取文件流

---
+++ 001java-base-mashibing/src/main/java/com/atguigu/java/ClassLoaderTest.java
@@ -0,0 +1,21 @@
+package com.atguigu.java;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ClassLoaderTest {
+	@Test
+	public void test1() throws Exception {
+		Properties p = new Properties();
+
+		//FileInputStream is = new FileInputStream("/jdbc.properties");
+
+
+		InputStream in = ClassLoaderTest.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		p.load(in);
+		String user = p.getProperty("user");
+		System.out.println("user = " + user);
+	}
+}
